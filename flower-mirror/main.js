d73d103e1edb674b186154b65209349774143bc5
Pim <pim@vicompany.nl>
2017-07-19 14:12:55 +0200

    flower mirror: drawImage

--- flower-mirror/main.js
+++ flower-mirror/main.js
@@ -1,8 +1,11 @@
 // Inspired by: https://www.openprocessing.org/sketch/85797
 // Which is also much nicer
 
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('.js-canvas');
 const ctx = canvas.getContext('2d');
+
+const canvas2 = document.querySelector('.js-duplicate');
+const ctx2 = canvas2.getContext('2d');
 
 const w = 500;
 const h = 500;
@@ -12,8 +15,8 @@ const midY = h >> 1;
 const PI = Math.PI;
 const TO_RADIAN = PI / 180;
 
-canvas.width = w;
-canvas.height = h;
+canvas.width = canvas2.width = w;
+canvas.height = canvas2.height = h;
 
 class Branch {
 	constructor(position = {x : 0, y: 0}, length = 100, divergeAt = 0.5, angle = 0, depth = 0, amp = 45 * TO_RADIAN) {
@@ -77,48 +80,24 @@ class Branch {
 	}
 
 	get canBranch() {
-		return this.depth < 3;
+		return this.depth < 4;
 	}
 }
 
 const branchPos = { x: midX, y: midY };
-const divergeAt = 0.25;
-
-const branches = [
-	new Branch(
-		branchPos,
-		midY,
-		divergeAt,
-		-90 * TO_RADIAN,
-		0
-	),
-	new Branch(
-		branchPos,
-		midY,
-		divergeAt,
-		0,
-		0
-	),
-	new Branch(
-		branchPos,
-		midY,
-		divergeAt,
-		90 * TO_RADIAN,
-		0
-	),
-	new Branch(
-		branchPos,
-		midY,
-		divergeAt,
-		180 * TO_RADIAN,
-		0
-	),
-];
-
+const divergeAt = 0.5;
+const rootBranch = new Branch(
+	branchPos,
+	midY,
+	divergeAt,
+	-90 * TO_RADIAN,
+	0
+);
 
 const drawBranch = (b) => {
 	const endX = b.length;
 	const endY = 0;
+	const r = 2;
 
 	ctx.save();
 	ctx.beginPath();
@@ -132,40 +111,64 @@ const drawBranch = (b) => {
 
 	ctx.stroke();
 	ctx.closePath();
+
+	ctx.beginPath();
+	ctx.arc(endX, endY, r, 0, PI * 2, false);
+	ctx.fill();
+	ctx.closePath();
+
 	ctx.restore();
 
 	b.branches.forEach(drawBranch);
 };
 
 const clear = () => {
-	ctx.clearRect(0, 0, ctx.canvas.width,ctx.canvas.height);
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	ctx2.clearRect(0, 0, ctx2.canvas.width, ctx2.canvas.height);
 };
 
 const loop = () => {
 	clear();
 
-	branches.forEach(drawBranch);
+	drawBranch(rootBranch);
+
+	const numSegments = 6;
+	const angleInc = PI * 2 / numSegments;
+	let angle = 0;
+
+	for (let i = 0; i < numSegments; i++) {
+		ctx2.save();
+		ctx2.translate(midX, midY);
+		ctx2.rotate(angle);
+		ctx2.drawImage(canvas, -w / 2, -h / 2);
+		ctx2.restore();
+
+		angle += angleInc;
+	}
+
+	ctx.drawImage(canvas2, 0, 0);
 
 	requestAnimationFrame(loop);
 };
 
 const onPointerMove = (e) => {
 	const target = (e.touches && e.touches.length) ? e.touches[0] : e;
-	const { clientX: x, clientY: y } = target;
+	const { offsetX: x, offsetY: y } = target;
 
-	const width = window.innerWidth;
-	const height = window.innerHeight;
+	const width = e.target.width;
+	const height = e.target.height;
 
-	const widthHalf = width / 2;
-	const ampX = ((x - widthHalf) / widthHalf) * (45 * TO_RADIAN);
+	const maxAmp = PI / 6; // 90 * TO_RADIAN;
+
+	// const widthHalf = width / 2;
+	// const ampX = ((x - widthHalf) / widthHalf) * (45 * TO_RADIAN);
+	const ampX = (x / width) * maxAmp;
 	const ampY = y / height;
 
-	branches.forEach((b) => {
-		b.update(ampX, ampY);
-	});
+	rootBranch.update(ampX, ampY);
 };
 
-document.addEventListener('mousemove', onPointerMove);
-document.addEventListener('touchmove', onPointerMove);
+canvas2.addEventListener('mousemove', onPointerMove);
+canvas2.addEventListener('touchmove', onPointerMove);
 
 loop();

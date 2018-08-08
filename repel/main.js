63e193e4e85baaab5d22748f13c4165f00ecabdc
Pim <pim@vicompany.nl>
2018-08-08 10:08:31 +0200

    repel

--- repel/main.js
+++ repel/main.js
@@ -3,7 +3,10 @@ const ctx = c.getContext('2d');
 
 const W = 500;
 const H = 500;
-const NUM_PARTICLES = 1500;
+const TAU = Math.PI * 2;
+const HYPO = Math.hypot(W, H);
+
+const NUM_PARTICLES = 2000;
 
 const particles = [];
 const mouse = new Vector(999, 999);
@@ -15,74 +18,71 @@ for (let i = 0; i < NUM_PARTICLES; i++) {
 	const x = Math.random() * W;
 	const y = Math.random() * H;
 
-	const acc = new Vector();
-
-	acc.length = 2 + Math.random();
-	acc.angle = 0;
-
 	const p = {
-		color: 'black',
-		force: 0,
 		pos: new Vector(x, y),
-		acc,
+		force: new Vector(),
+		acc:  new Vector(1, 0),
+		vel: new Vector(0, 0),
 	};
 
 	particles.push(p);
 }
 
-const distanceBetween = (vec1, vec2) => {
-	const x = vec2.x - vec1.x;
-	const y = vec2.y - vec1.y;
-
-	return Math.hypot(x, y);
-};
-
-const angleBetween = (vec1, vec2) => {
-	const x = vec2.x - vec1.x;
-	const y = vec2.y - vec1.y;
-
-	return Math.atan2(y, x);
-};
-
+const distanceBetween = (vec1, vec2) => Math.hypot(vec2.x - vec1.x, vec2.y - vec1.y);
+const angleBetween = (vec1, vec2) => Math.atan2(vec2.y - vec1.y, vec2.x - vec1.x);
 const clamp = (value, min, max) => Math.max(min, Math.min(value, max));
 
 const clear = () => {
-	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 };
 
 const update = (particle) => {
-	const falloff = 100;
+	const { pos, acc, vel } = particle;
 
-	const { pos, acc } = particle;
+	const falloff = 50;
 
-	pos.addSelf(acc);
+	const distance = distanceBetween(mouse, particle.pos);
+	const force = 1 - clamp(distance / falloff, 0, 1);
+	const angle = angleBetween(mouse, particle.pos);
 
-	const distance =  distanceBetween(pos, mouse);
-	const angle = angleBetween(pos, mouse);
-	const force = (falloff - clamp(distance, 0, falloff)) / falloff;
+	const strength = 0.75;
 
-	particle.color = distance < falloff ? 'red' : 'black';
-	particle.force = force;
-	particle.acc.angle = -angle * force;
+	particle.force.x = Math.cos(angle) * force * strength;
+	particle.force.y = Math.sin(angle) * force * strength;
 
-	if (pos.x > W) {
-		pos.x = 0;
-	} else if (pos.x < 0) {
-		pos.x = W;
+	particle.acc.addSelf(particle.force);
+	particle.vel.addSelf(particle.acc);
+	particle.pos.addSelf(particle.vel);
+
+	particle.acc.multiplySelf(0);
+	particle.vel.limit(1);
+
+	particle.distance = clamp(1 - (distance / (falloff * 3)), 0, 1);
+
+	const padding = 0;
+
+	if (pos.x > W - padding) {
+		pos.x = padding;
+	} else if (pos.x < padding) {
+		pos.x = W - padding;
 	}
 
-	if (pos.y > H) {
-		pos.y = 0;
-	} else if (pos.y < 0) {
-		pos.y = H;
+	if (pos.y > H - padding) {
+		pos.y = padding;
+	} else if (pos.y < padding) {
+		pos.y = H - padding;
 	}
+
 };
 
 const draw = (particle) => {
-	ctx.beginPath();
-	ctx.fillStyle = particle.color;
+	const { distance } = particle;
 
-	ctx.arc(particle.pos.x, particle.pos.y, 2, 0, Math.PI * 2, false);
+	ctx.beginPath();
+	ctx.fillStyle = `rgba(${distance * 255}, 0, 0, 1)`;
+
+	ctx.arc(particle.pos.x, particle.pos.y, 1, 0, TAU, false);
 	ctx.fill();
 	ctx.closePath();
 };

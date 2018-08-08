62d4ad869994b06a9bb28e22ce26cb458d28eeca
Pim <pim@vicompany.nl>
2018-08-08 10:39:30 +0200

    imagedata

--- repel/main.js
+++ repel/main.js
@@ -1,15 +1,16 @@
 const c = document.querySelector('.js-canvas');
 const ctx = c.getContext('2d');
 
-const W = 500;
-const H = 500;
+const W = 400;
+const H = W;
 const TAU = Math.PI * 2;
 const HYPO = Math.hypot(W, H);
 
-const NUM_PARTICLES = 2000;
+const NUM_PARTICLES = 8000;
 
 const particles = [];
 const mouse = new Vector(999, 999);
+const imageData = ctx.getImageData(0, 0, W, H);
 
 c.width = W;
 c.height = H;
@@ -28,13 +29,21 @@ for (let i = 0; i < NUM_PARTICLES; i++) {
 	particles.push(p);
 }
 
+const pixelIndex = (x, y, imageData) => (~~x + ~~y * imageData.width) * 4;
 const distanceBetween = (vec1, vec2) => Math.hypot(vec2.x - vec1.x, vec2.y - vec1.y);
 const angleBetween = (vec1, vec2) => Math.atan2(vec2.y - vec1.y, vec2.x - vec1.x);
 const clamp = (value, min, max) => Math.max(min, Math.min(value, max));
 
-const clear = () => {
+const clearStage = () => {
 	ctx.fillStyle = 'rgba(255, 255, 255, 1)';
 	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+};
+
+const clear = (particle) => {
+	const { pos } = particle;
+	const index = pixelIndex(pos.x, pos.y, imageData);
+
+	imageData.data[index + 3] = 0;
 };
 
 const update = (particle) => {
@@ -77,24 +86,27 @@ const update = (particle) => {
 };
 
 const draw = (particle) => {
-	const { distance } = particle;
+	const { distance, pos } = particle;
 
-	ctx.beginPath();
-	ctx.fillStyle = `rgba(${distance * 255}, 0, 0, 1)`;
+	const index = pixelIndex(pos.x, pos.y, imageData);
 
-	ctx.arc(particle.pos.x, particle.pos.y, 1, 0, TAU, false);
-	ctx.fill();
-	ctx.closePath();
+	imageData.data[index] = distance * 255;
+	imageData.data[index + 1] = 0;
+	imageData.data[index + 2] = 0;
+	imageData.data[index + 3] = 255;
 };
 
 
 const loop = () => {
-	clear();
-
 	particles.forEach((p) => {
+		clear(p);
 		update(p);
 		draw(p);
 	});
+
+	ctx.scale(1.05, 1.05);
+
+	ctx.putImageData(imageData, 0, 0);
 
 	requestAnimationFrame(loop);
 };

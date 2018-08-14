fe47a662d29efe8a27da6688cf033253d35e18e4
Pim <pim@vicompany.nl>
2018-08-14 10:51:48 +0200

    glendmode

--- waterpaint/main.js
+++ waterpaint/main.js
@@ -8,28 +8,28 @@
 // https://p5js.org/reference/#/p5/randomGaussian
 // https://github.com/processing/p5.js/blob/master/src/math/random.js#L166
 const randomGaussian = (mean = 0, sd = 1) => {
-  let y1;
-  let x1;
-  let x2
-  let w;
-  let previous;
+	let y1;
+	let x1;
+	let x2
+	let w;
+	let previous;
 
-  if (previous) {
-    y1 = y2;
-    previous = false;
-  } else {
-    do {
-      x1 = (Math.random() * 2) - 1;
-      x2 = (Math.random() * 2) - 1;
-      w = x1 * x1 + x2 * x2;
-    } while (w >= 1);
-    w = Math.sqrt(-2 * Math.log(w) / w);
-    y1 = x1 * w;
-    y2 = x2 * w;
-    previous = true;
-  }
+	if (previous) {
+		y1 = y2;
+		previous = false;
+	} else {
+		do {
+			x1 = (Math.random() * 2) - 1;
+			x2 = (Math.random() * 2) - 1;
+			w = x1 * x1 + x2 * x2;
+		} while (w >= 1);
+		w = Math.sqrt(-2 * Math.log(w) / w);
+		y1 = x1 * w;
+		y2 = x2 * w;
+		previous = true;
+	}
 
-  return y1 * sd + mean;
+	return y1 * sd + mean;
 };
 
 const randomArrayValue = arr => arr[Math.floor(Math.random() * arr.length)];
@@ -54,76 +54,72 @@ const COLORS = [
 	`hsla(330, 100%, 50%, ${ALPHA})`,
 	`hsla(210, 100%, 50%, ${ALPHA})`,
 	`hsla(140, 100%, 50%, ${ALPHA})`,
+	`hsla(0, 100%, 50%, ${ALPHA})`,
 ];
 
 c.width = W;
 c.height = H;
 
 const getPoly = (midX, midY, r, edges) => {
-  const vertices = [];
-  const angleStep = TAU / edges;
+	const vertices = [];
+	const angleStep = TAU / edges;
 
-  for (let angle = 0; angle < TAU; angle += angleStep) {
-    const x = midX + Math.cos(angle) * r;
-    const y = midY + Math.sin(angle) * r;
+	for (let angle = 0; angle < TAU; angle += angleStep) {
+		const x = midX + Math.cos(angle) * r;
+		const y = midY + Math.sin(angle) * r;
 
-    vertices.push({ x, y });
-  }
+		vertices.push({ x, y });
+	}
 
-  return vertices;
+	return vertices;
 };
 
 const deformPoly = (vertices, depth, variance, varianceDecrease) => {
-  let deformedVertices = [];
+	let deformedVertices = [];
 
-  for (let i = 0; i < vertices.length; i++) {
-    const from = vertices[i];
-    const to = i === vertices.length - 1 ? vertices[0] : vertices[i + 1];
+	for (let i = 0; i < vertices.length; i++) {
+		const from = vertices[i];
+		const to = i === vertices.length - 1 ? vertices[0] : vertices[i + 1];
 
-    const midX = (from.x + to.x) * 0.5;
-    const midY = (from.y + to.y) * 0.5;
-    const angle = angleBetween(
-      { x: 0, y: 0 },
-      { x: midX, y: midY },
-    );
+		const midX = (from.x + to.x) * 0.5;
+		const midY = (from.y + to.y) * 0.5;
+		const angle = angleBetween(
+			{ x: 0, y: 0 },
+			{ x: midX, y: midY },
+		);
 
-    const newX = midX + randomGaussian() * variance;
-    const newY = midY + randomGaussian() * variance;
+		const newX = midX + randomGaussian() * variance;
+		const newY = midY + randomGaussian() * variance;
 
-    deformedVertices.push(from);
-    deformedVertices.push({ x: newX, y: newY });
-  }
+		deformedVertices.push(from);
+		deformedVertices.push({ x: newX, y: newY });
+	}
 
-  if (depth > 0) {
-    depth--;
+	if (depth > 0) {
+		depth--;
 
-    return deformPoly(deformedVertices, depth, variance / varianceDecrease, varianceDecrease);
-  }
+		return deformPoly(deformedVertices, depth, variance / varianceDecrease, varianceDecrease);
+	}
 
-  return deformedVertices;
+	return deformedVertices;
 };
 
 const VARIANCE_DEFAULT = R / 15;
 const NUM_POLIES = 100;
 const NUM_SPOTS = 3;
 const DEPTH = 5;
-const DISPLACEMENT = 75;
+const DISPLACEMENT = R / 1.5;
 
-const stacklist = new Array(NUM_SPOTS).fill().map((_, i) => {
+const polies = new Array(NUM_SPOTS).fill().map((_, i) => {
 	const poly = getPoly(
 		randomBetween(-DISPLACEMENT, DISPLACEMENT),
 		randomBetween(-DISPLACEMENT, DISPLACEMENT),
 		R + randomBetween(-15, 15),
 		EDGES
 	);
-	const polyDeformed = deformPoly(poly, DEPTH, VARIANCE_DEFAULT, 2);
-	const stack = new Array(NUM_POLIES).fill().map((_, i) => deformPoly(polyDeformed, DEPTH, VARIANCE_DEFAULT + randomBetween(5, 15), 4));
 
-	return stack;
+	return deformPoly(poly, DEPTH, VARIANCE_DEFAULT, 2);
 });
-
-let layer = 0;
-const interleave = 5;
 
 const drawSpot = (vertex, r, color) => {
 	ctx.beginPath();
@@ -134,60 +130,49 @@ const drawSpot = (vertex, r, color) => {
 };
 
 const drawPoly = (vertices, color) => {
-  const [firstVertex] = vertices;
+	const [firstVertex] = vertices;
 
-  ctx.beginPath();
-  ctx.fillStyle = color;
-  ctx.moveTo(firstVertex.x, firstVertex.y);
-
-  for (let i = 1; i < vertices.length; i++) {
-    const vertex = vertices[i];
-    ctx.lineTo(vertex.x, vertex.y);
-  }
-
-  ctx.lineTo(firstVertex.x, firstVertex.y);
-  ctx.fill();
-  ctx.closePath();
-};
-
-const drawLayer = (stack, stackIndex, layerStartIndex, layerEndIndex, color) => {
 	ctx.save();
 	ctx.translate(MID_X, MID_Y);
-  ctx.rotate((Math.PI / 2) * stackIndex);
+	ctx.beginPath();
+	ctx.fillStyle = color;
+	ctx.moveTo(firstVertex.x, firstVertex.y);
 
-  for (let i = layerStartIndex; i < layerEndIndex; i++) {
-    drawPoly(stack[i], color);
+	for (let i = 1; i < vertices.length; i++) {
+		const vertex = vertices[i];
+		ctx.lineTo(vertex.x, vertex.y);
 	}
 
-	if (Math.round(Math.random() * 10) === 0) {
-		const polyIndex = layerStartIndex;
+	ctx.lineTo(firstVertex.x, firstVertex.y);
+	ctx.fill();
+	ctx.closePath();
+	ctx.restore();
+};
 
-		const poly = stack[polyIndex];
-		const vertex = randomArrayValue(poly);
-		const r = randomBetween(2, 6);
 
-		drawSpot(vertex, r, color);
+let polyCount = 0;
+const interleave = 5;
+
+const drawLayer = (poly, polyIndex) => {
+	for (let i = 0; i < interleave; i++) {
+		requestAnimationFrame(() => {
+			const deformed = deformPoly(poly, DEPTH, VARIANCE_DEFAULT + randomBetween(5, 15), 4);
+
+			drawPoly(deformed, COLORS[polyIndex]);
+		});
 	}
-
-  ctx.restore();
 };
 
-const drawStackList = (stacklist, colors, interleave) => {
-  stacklist.forEach((stack, stackIndex) => {
-    for (let i = layer; i < layer + interleave; i++) {
-      const to = Math.min(layer + interleave, NUM_POLIES - 2);
+const draw = () => {
+	polies.forEach(drawLayer);
 
-      drawLayer(stack, stackIndex, i, to, colors[stackIndex]);
-    }
-  });
+	polyCount += interleave;
 
-  layer += interleave;
-
-  if (layer < NUM_POLIES) {
-    requestAnimationFrame(() => {
-      drawStackList(stacklist, colors, interleave);
-    });
-  }
+	if (polyCount < 200) {
+		requestAnimationFrame(draw);
+	}
 };
 
-drawStackList(stacklist, COLORS, interleave);
+ctx.globalCompositeOperation = 'overlay';
+
+draw();

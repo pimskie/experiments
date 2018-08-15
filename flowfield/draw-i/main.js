92da5cc2d20e647576bd8bc7ba0811438a5af1f0
Pim <pim@vicompany.nl>
2018-08-15 14:24:48 +0200

    save color in point

--- flowfield/draw-i/main.js
+++ flowfield/draw-i/main.js
@@ -18,8 +18,9 @@ const PALETTES = [
 	'https://coolors.co/app/9c89b8-f0a6ca-efc3e6-f0e6ef-b8bedd',
 ];
 
-const NOISE_SCALE = 0.02;
-const NUM_POINTS = 250;
+const NOISE_SCALE = 0.005;
+const ANGLE_SCALE = 0.004;
+const NUM_POINTS = 500;
 
 let colors = [];
 let points = [];
@@ -48,27 +49,21 @@ const clear = () => {
 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 };
 
-const ANGLE_SCALE = 0.005;
-
 const draw = () => {
 	points.forEach((point, index) => {
-		const colorNoise = noise.simplex2(index * NOISE_SCALE, index * NOISE_SCALE);
-		const colorNoiseValue = Math.round(Math.abs(colorNoise * 100));
+		const { pos, color } = point;
 
-		const colorIndex = (colorNoiseValue + colors.length) % colors.length;
-		const color = colors[colorIndex];
-
-		const angleNoise = noise.perlin2(point.x * ANGLE_SCALE, point.y * ANGLE_SCALE, time);
+		const angleNoise = noise.perlin2(pos.x * ANGLE_SCALE, pos.y * ANGLE_SCALE, time);
 		const angle = TAU * angleNoise;
 		const vector = new Vector(Math.cos(angle), Math.sin(angle));
 
-		point.addSelf(vector);
+		pos.addSelf(vector);
 
-		drawPoint(ctx, point, color);
+		drawPoint(ctx, pos, color);
 
-		if (point.x > W || point.x < 0 || point.Y > H || point.y < 0) {
-			point.x = Utils.randomGaussian() * W;
-			point.y = Utils.randomGaussian() * H;
+		if (pos.x > W || pos.x < 0 || pos.Y > H || pos.y < 0) {
+			pos.x = Utils.randomGaussian() * W;
+			pos.y = Utils.randomGaussian() * H;
 		}
 	});
 
@@ -86,14 +81,26 @@ const run = () => {
 	cancelAnimationFrame(rafId);
 
 	noise.seed(Math.random());
-
 	colors = getColors();
 
-	points = new Array(NUM_POINTS).fill().map(() => {
+	points = new Array(NUM_POINTS).fill().map((_, index) => {
 		const x = Utils.randomGaussian() * W;
 		const y = Utils.randomGaussian() * H;
+		const pos = new Vector(x, y);
+		const mass = Utils.randomBetween(1, 2);
 
-		return new Vector(x, y);
+		const colorNoise = noise.simplex2(index * NOISE_SCALE, index * NOISE_SCALE);
+		const colorNoiseValue = Math.round(Math.abs(colorNoise * 100));
+		const colorIndex = (colorNoiseValue + colors.length) % colors.length;
+		const color = colors[colorIndex];
+
+		return {
+			pos,
+			mass,
+			color,
+			colorIndex,
+		};
+
 	});
 
 	loop();

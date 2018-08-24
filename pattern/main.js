d5e09907b1f19b91063a31a2773ad619fb93a13c
Pim <pim@vicompany.nl>
2018-08-24 17:14:52 +0200

    trails

--- pattern/main.js
+++ pattern/main.js
@@ -8,20 +8,90 @@ const TAU = PI * 2;
 
 const W = 500;
 const H = 500;
-const MID_X = W * 0.5;
-const MID_Y = H * 0.5;
 
 ctx.canvas.width = W;
 ctx.canvas.height = H;
 
-const LINE_WIDTH = 30;
-const R = 30;
+const LINE_WIDTH = 10;
+const R = 10;
 const R2 = R * 2;
-const CIRCLE_R = R;
+
+let trails = [];
+let frame = 0;
+let rafId = null;
+
+const shuffleArray = arie => arie
+	.map(a => [Math.random(), a])
+	.sort((a, b) => a[0] - b[0])
+	.map(a => a[1]);
+
+class Trail {
+	constructor(x, y, colors, angleInc) {
+		this.x = x;
+		this.y = y;
+
+		this.colors = colors;
+		this.angleInc = angleInc;
+
+		this.startAngle = 0;
+		this.endAngle = this.startAngle + this.angleInc;
+
+		this.antiClockwise = false;
+	}
+
+	iterate() {
+		const changeDirection = Math.random() > 0.5;
+
+		if (changeDirection) {
+			this.acw = !this.acw;
+		}
+
+		this.startAngle = changeDirection
+			? this.endAngle - PI
+			: this.endAngle;
+
+		if (this.startAngle === -PI) {
+			this.startAngle = PI;
+		}
+
+		this.endAngle = this.acw
+			? this.startAngle - this.angleInc
+			: this.startAngle + this.angleInc;
+
+		if (changeDirection) {
+			this.x -= Math.cos(this.startAngle) * R2;
+			this.y -= Math.sin(this.startAngle) * R2;
+		}
+
+		if (Math.abs(this.startAngle) === TAU) {
+			this.endAngle = -this.angleInc;
+			this.startAngle = 0;
+		}
+
+		this.draw(this.x, this.y, this.startAngle, this.endAngle, this.acw);
+	}
+
+	draw(x, y, startAngle, endAngle, acw = false) {
+		const [outside, inside] = this.colors;
+
+		this.drawSegment(x, y, startAngle, endAngle, acw, LINE_WIDTH, outside);
+		this.drawSegment(x, y, startAngle, endAngle, acw, LINE_WIDTH * 0.75, inside);
+	}
+
+	drawSegment(x, y, startAngle, endAngle, acw, lineWidth, color) {
+		ctx.beginPath();
+		ctx.lineWidth = lineWidth;
+		ctx.strokeStyle = color;
+		ctx.arc(x, y, R, startAngle, endAngle, acw);
+		ctx.stroke();
+		ctx.closePath();
+	}
+}
 
 const drawLine = (from, to) => {
 	ctx.beginPath();
 	ctx.lineWidth = 0.5;
+	ctx.strokeStyle = 'rgba(0, 0, 0, 0.2)';
 
 	ctx.moveTo(from.x, from.y);
 	ctx.lineTo(to.x, to.y);
@@ -48,76 +118,56 @@ const drawGrid = (width, height, size) => {
 	}
 };
 
-const drawSegment = (x, y, startAngle, endAngle, acw, lineWidth, color) => {
-	ctx.beginPath();
-	ctx.lineWidth = lineWidth;
-	ctx.strokeStyle = color;
-	ctx.arc(x, y, CIRCLE_R, startAngle, endAngle, acw);
-	ctx.stroke();
-	ctx.closePath();
-};
-
-const drawShape = (x, y, startAngle, endAngle, acw = false) => {
-	ctx.strokeStyle = '#' + ((1<<24)*Math.random()|0).toString(16);
-
-	drawSegment(x, y, startAngle, endAngle, acw, LINE_WIDTH, 'black');
-	drawSegment(x, y, startAngle, endAngle, acw, LINE_WIDTH - 4, 'white');
-};
-
-
-drawGrid(W, H, R);
-
-let x = MID_X - R;
-let y = MID_Y - R;
-let startAngle = 0;
-let endAngle = startAngle + QUART;
-let acw = false;
-
-drawShape(x, y, startAngle, endAngle, false);
-
-const iterate = (changeDir) => {
-	if (changeDir) {
-		acw = !acw;
-	}
-
-	startAngle = changeDir ? endAngle - PI : endAngle;
-
-	if (startAngle === -PI) {
-		startAngle = PI;
-	}
-
-	endAngle = acw ? startAngle - QUART : startAngle + QUART;
-
-	if (changeDir) {
-		x -= Math.cos(startAngle) * R2;
-		y -= Math.sin(startAngle) * R2;
-	}
-
-	if (Math.abs(startAngle) === TAU) {
-		endAngle = -QUART;
-		startAngle = 0;
-	}
-
-
-	drawShape(x, y, startAngle, endAngle, acw);
-
-	// console.log(acw, Utils.toDegrees(startAngle), Utils.toDegrees(endAngle));
-
-};
 
 const clear = () => {
 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 };
 
-const loop = () => {
-	// clear();
+const init = () => {
+	clear();
 
-	let changeDir = Math.random() > 0.4;
+	// drawGrid(W, H, R);
 
+	trails = [];
+	frame = 0;
 
-	iterate(changeDir);
+	cancelAnimationFrame(rafId);
 
-	requestAnimationFrame(loop);
+	for (let i = 0; i < 15; i++) {
+		const t1 = new Trail(Math.random() * W, Math.random() * H, ['#6E8898', '#9FB1BC'], QUART);
+		const t2 = new Trail(Math.random() * W, Math.random() * H, ['#1A535C', '#F7FFF7'], QUART);
+		const t3 = new Trail(Math.random() * W, Math.random() * H, ['#A23E48', '#FF3C38'], QUART);
+
+		trails.push(t1);
+		trails.push(t2);
+		trails.push(t3);
+	}
+
+	loop();
 };
 
+const loop = () => {
+	trails.forEach((trail) => {
+		if (trail.x < -R || trail.x > W + R || trail.y < -R || trail.y > H + R) {
+			// trail.x = MID_X - R;
+			// trail.y = MID_Y - R;
+		}
+
+		trail.iterate();
+		trail.draw();
+	});
+
+	shuffleArray(trails);
+
+	frame++;
+
+	if (frame < 500) {
+		rafId = requestAnimationFrame(loop);
+	}
+};
+
+
+ctx.canvas.addEventListener('click', init);
+
+init();
 loop();

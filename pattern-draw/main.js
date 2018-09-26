ffccfaca6fc70a8a0c239081d150f0dac86abde1
Pim <pim@vicompany.nl>
2018-09-26 15:05:07 +0200

    tweakie

--- pattern-draw/main.js
+++ pattern-draw/main.js
@@ -9,94 +9,87 @@ const W = 500;
 const H = 500;
 const MID_X = W * 0.5;
 const MID_Y = H * 0.5;
-const RADIUS = MID_X - 25;
+const RADIUS = MID_X * 0.5;
 
 ctx.canvas.width = W;
 ctx.canvas.height = H;
 
-const ANIM_DURATION = 7000;
-
 let phase = 0;
-let startTime = performance.now();
+let paused = false;
+
+const connectionStep = 2;
 
 const minimumPoints = 4;
 const maxPoints = 10;
 const angleStepMinumum = Math.PI * 2 / minimumPoints;
-const angleStep = Math.PI * 2 / maxPoints;
-const angleStepDiff = angleStep - angleStepMinumum;
+const angleStepMaximum = Math.PI * 2 / maxPoints;
+const angleStepDiff = angleStepMaximum - angleStepMinumum;
 
-//https://gist.github.com/gre/1650294
-const easeInOutCubic = t => t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+const loop = () => {
+	ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+	ctx.fillRect(0, 0, W, H);
 
-(() => {
-	const loop = () => {
-		ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-		ctx.fillRect(0, 0, W, H);
+	const percent = Utils.map(Math.sin(phase) * 100, -100, 100, 0, 1);
 
-		const now = performance.now();
+	const points = new Array(maxPoints + 1)
+		.fill()
+		.map((_, i) => {
+			const angle = (i * angleStepMinumum + (angleStepDiff * i * percent)) % (Math.PI * 2);
+			const radius = (RADIUS - 100) + (100 * percent);
 
-		const percent = Utils.map(Math.sin(phase) * 100, -100, 100, 0, 1);
-		const radiusModifier = Utils.map(Math.sin(phase) * 100, -100, 100, -25, 25);
-
-		const points = new Array(maxPoints + 1).fill().map((_, i) => {
-			let angle = (i * angleStepMinumum + (angleStepDiff * i * percent)) % (Math.PI * 2);
-
-			const x = MID_X + Math.cos(angle) * (RADIUS + radiusModifier);
-			const y = MID_Y + Math.sin(angle) * (RADIUS + radiusModifier);
+			const x = MID_X + Math.cos(angle) * radius;
+			const y = MID_Y + Math.sin(angle) * radius;
 
 			return { x, y, angle };
-		}).sort((a, b) => a.angle > b.angle ? 1 : a.angle < b.angle ? -1 : 0);
+		})
+		.sort((a, b) => a.angle > b.angle ? 1 : a.angle < b.angle ? -1 : 0)
+		.splice(1);
 
-		points.shift();
+
+	ctx.beginPath();
+	ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+
+	points.forEach((point, i) => {
+		const { x, y } = point;
+
+		if (i === 0) {
+			ctx.moveTo(x, y);
+		} else {
+			ctx.lineTo(x, y);
+		}
+	});
+	ctx.closePath();
+	ctx.stroke();
+
+	points.forEach((point, i) => {
+		const indexTo = (i + connectionStep + points.length) % points.length;;
+		const pointTo = points[indexTo];
+
+		const { x, y } = point;
+		const { x: xTo, y: yTo } = pointTo;
 
 		ctx.beginPath();
-
-		points.forEach((point, i) => {
-			const { x, y } = point;
-
-			if (i === 0) {
-				ctx.moveTo(x, y);
-			} else {
-				ctx.lineTo(x, y);
-			}
-		});
-
+		ctx.fillStyle = '#000';
+		ctx.arc(x, y, 2, 0, Math.PI * 2, false);
+		ctx.fill();
 		ctx.closePath();
+
+		// connection
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(xTo, yTo);
 		ctx.stroke();
+		ctx.closePath();
+	});
 
-		const connectionStep = 4;
+	if (!paused) {
+		phase += 0.008;
+	}
 
-		points.forEach((point, i) => {
-			const indexTo = (i + connectionStep + points.length) % points.length;;
-			const pointTo = points[indexTo];
+	requestAnimationFrame(loop);
+};
 
-			const { x, y } = point;
-			const { x: xTo, y: yTo } = pointTo;
-
-			ctx.beginPath();
-			ctx.fillStyle = '#000';
-			ctx.arc(x, y, 2, 0, Math.PI * 2, false);
-			ctx.fill();
-			ctx.closePath();
-
-			ctx.beginPath();
-			ctx.moveTo(x, y);
-			ctx.lineTo(xTo, yTo);
-			ctx.stroke();
-			ctx.closePath();
-		});
-
-
-		if (now - startTime >= ANIM_DURATION) {
-			startTime += ANIM_DURATION;
-		}
-
-		phase += 0.005;
-		requestAnimationFrame(loop);
-	};
-
-	loop();
-})();
+loop();
 
 const onPointerMove = (e) => {
 	const event = (e.touches && e.touches.length) ? e.touches[0] : e;
@@ -109,5 +102,10 @@ const onPointerMove = (e) => {
 	const percentY = y / canvas.height;
 };
 
+const onPointerDown = () => {
+	paused = !paused;
+};
+
 canvas.addEventListener('mousemove', onPointerMove);
 canvas.addEventListener('touchmove', onPointerMove);
+canvas.addEventListener('mousedown', onPointerDown);

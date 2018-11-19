ee2c01599e5e838ec68b8e32d464b1a4e3b98b18
Pim <pim@vicompany.nl>
2018-11-19 14:12:42 +0100

    animating

--- action-reaction/octa-anim/main.js
+++ action-reaction/octa-anim/main.js
@@ -1,6 +1,7 @@
 import * as Utils from 'https://rawgit.com/pimskie/utils/master/utils.js';
 
-const ctx = Utils.qs('canvas').getContext('2d');
+const ctx = Utils.qs('.js-canvas').getContext('2d');
+const ctxGhost = Utils.qs('.js-canvas-ghost').getContext('2d');
 
 const W = Utils.clamp(window.innerWidth - 150, 200, 500);
 const H = W;
@@ -10,11 +11,39 @@ const MID_Y = H * 0.5;
 
 const PIPI = Math.PI * 2;
 
+const MAGIC_SCALE_NUMBER = 0.76;
+
 ctx.canvas.width = W;
 ctx.canvas.height = H;
+ctxGhost.canvas.width = W;
+ctxGhost.canvas.height = H;
 
-const draw = (points, iteration = 0, scale = 1) => {
-	const color = iteration % 2 === 0 ? '#000' : '#ff0000';
+const NUM_EDGES = 6;
+const NUM_SHAPES = 100;
+
+const points = new Array(NUM_EDGES).fill().map((_, i) => {
+	const x = Math.cos(i * (PIPI / NUM_EDGES)) * MID_X;
+	const y = Math.sin(i * (PIPI / NUM_EDGES)) * MID_X;
+
+	return { x, y };
+});
+
+const shapes = new Array(NUM_SHAPES).fill().map((_, i) => {
+	const shapeScale = Math.pow(MAGIC_SCALE_NUMBER, i);
+	const rotation = i * 45 * (180 / Math.PI);
+
+	const shape = {
+		points,
+		rotation,
+		scale: shapeScale,
+		scaleOrig: shapeScale,
+	};
+
+	return shape;
+});
+
+const draw = (ctx, points, iteration = 0, scale = 1) => {
+	const color = iteration % 2 === 0 ? '#000' : '#fff';
 	const rotation = (iteration + 1) * 90 * (Math.PI / 180);
 
 	ctx.fillStyle = color;
@@ -37,60 +66,40 @@ const draw = (points, iteration = 0, scale = 1) => {
 	ctx.restore();
 };
 
-const NUM_EDGES = 6;
-const NUM_SHAPES = 100
-let scale = 1;
-
-const points = new Array(NUM_EDGES).fill().map((_, i) => {
-	const x = Math.cos(i * (PIPI / NUM_EDGES)) * MID_X;
-	const y = Math.sin(i * (PIPI / NUM_EDGES)) * MID_X;
-
-	return { x, y };
-});
-
-const shapes = new Array(NUM_SHAPES).fill().map((_, i) => {
-	const shape = {
-		points,
-		scale,
-		scaleOrig: scale,
-	};
-
-	scale *= 0.86;
-
-	return shape;
-});
-
-const clear = () => {
+const clear = (ctx) => {
 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 };
 
+// using `xor` to make inner transparent
+draw(ctxGhost, points, 0, 1);
+draw(ctxGhost, points, 1, 0.86);
+
+const duplicate = (scale, rotation) => {
+	ctx.save();
+	ctx.translate(MID_X, MID_Y);
+
+	ctx.scale(scale, scale);
+
+	ctx.rotate(rotation);
+	ctx.drawImage(ctxGhost.canvas, -MID_X, -MID_Y);
+
+	ctx.restore();
+};
+
 const loop = () => {
-	clear();
+	clear(ctx);
 
 	for (let i = 0; i < shapes.length; i++) {
 		const shape = shapes[i];
-		const { points, scale } = shape;
 
-		draw(points, i, scale);
+		shape.scale *= 1.01;
 
-		shape.scale *= 1.004;
+		duplicate(shape.scale, shape.rotation);
 
-		if (shape.scale >= 2) {
-			shape.scale = 0.01;
+		if (shape.scale>= 2) {
+			shape.scale = Math.pow(MAGIC_SCALE_NUMBER, shapes.length);
 		}
 	}
-
-	shapes.sort((a, b) => {
-		if (a.scale < b.scale) {
-			return 1;
-		}
-
-		if (a.scale > b.scale) {
-			return -1;
-		}
-
-		return 0;
-	});
 
 	requestAnimationFrame(loop);
 };

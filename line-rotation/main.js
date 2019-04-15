ae4121388e7d43fee4bc684ff42c8d615e2dc26f
Pim <pim@vicompany.nl>
2019-04-15 16:16:07 +0200

    styling

--- line-rotation/main.js
+++ line-rotation/main.js
@@ -1,6 +1,7 @@
+import anime from '//unpkg.com/animejs@3.0.1/lib/anime.es.js';
+
 // https://ptsjs.org/
 const TAU = Math.PI * 2;
-const PERP = Math.PI / 2;
 
 const canvas = document.querySelector('.js-draw');
 
@@ -14,41 +15,35 @@ class Stage {
 
 		this.hypo = Math.hypot(width, height);
 
-		this.mouseAngle = 0;
+		this.options = {
+			rotation: Math.atan2(width, height) + Math.PI / 2,
+			backgroundColor: '#011627',
+			lineColor: '#FDFFFC',
+			pointColor1: '#41EAD4',
+			pointColor2: '#F71735',
+		};
+
 		this.points = [];
 
 		this.line = { from: { x: 0, y: height * 0.5 }, to: { x: width, y: height * 0.5 } };
 
-		this.colors = [['#fff', '#000', '#000', '#000','#000']];
-		this.palette = this.colors[0];
+		this.animation = null;
 	}
 
-	get backgroundColor() {
-		return '#011627'; // this.palette[0];
-	}
-
-	get lineColor() {
-		return '#FDFFFC'; // this.palette[4];
-	}
-
-	get pointColor1() {
-		return '#41EAD4'; // this.palette[3];
-	}
-
-	get pointColor2() {
-		return '#F71735';
+	get rotation() {
+		return this.options.rotation;
 	}
 
 	get width() {
 		return this.canvas.width;
 	}
 
-	get widthHalf() {
-		return this.width * 0.5;
-	}
-
 	get height() {
 		return this.canvas.height;
+	}
+
+	get widthHalf() {
+		return this.width * 0.5;
 	}
 
 	get heightHalf() {
@@ -56,13 +51,11 @@ class Stage {
 	}
 
 	async init() {
-		this.colors = await Stage.getColors();
-
 		this.points = new Array(100).fill().map((_, i) => {
 			const r = (this.heightHalf * 0.5) + Math.random() * (this.heightHalf / 2);
 			const p = r / this.heightHalf;
 			const o = p;
-			const c = i % 2 === 0 ? this.pointColor1 : this.pointColor2;
+			const c = i % 2 === 0 ? this.options.pointColor1 : this.options.pointColor2;
 
 			const point = {
 				r,
@@ -75,35 +68,48 @@ class Stage {
 			return point;
 		});
 
-		this.setPalette();
-
 		this.canvas.addEventListener('mousemove', e => this.onMouseMove(e));
-		this.canvas.addEventListener('mouseup', () => this.setPalette());
+		this.canvas.addEventListener('mouseenter', e => this.onMoueEnter(e));
+		this.canvas.addEventListener('mouseleave', e => this.onMouseLeave(e));
 	}
 
 	onMouseMove(e) {
-		const { width, height } = this.canvas;
-		const widthHalf = width * 0.5;
-		const heightHalf = height * 0.5;
-		const radius = width;
-
 		const x = e.clientX - e.target.offsetLeft;
 		const y = e.clientY - e.target.offsetTop;
 
-		this.mouseAngle = Math.atan2(this.heightHalf - y, this.widthHalf - x);
-
-		this.line.from.x = widthHalf + (Math.cos(this.mouseAngle) * radius);
-		this.line.from.y = heightHalf + (Math.sin(this.mouseAngle) * radius);
-		this.line.to.x = widthHalf + (Math.cos(this.mouseAngle + Math.PI) * radius);
-		this.line.to.y = heightHalf + (Math.sin(this.mouseAngle + Math.PI) * radius);
+		this.options.rotation = Math.atan2(this.heightHalf - y, this.widthHalf - x);
 	}
 
-	setPalette() {
-		this.palette = Stage.getPalette(this.colors);
+	onMoueEnter() {
+		if (this.animation) {
+			this.animation.pause();
+		}
+
+		this.animation = null;
+	}
+
+	onMouseLeave() {
+		this.animate();
+	}
+
+	animate() {
+		const diff = -Math.PI + (Math.random() * TAU);
+		const angle = this.rotation + diff;
+
+		this.animation = anime({
+			targets: this.options,
+			duration: 3000,
+			delay: 3000,
+			rotation: angle,
+			easing: 'easeInOutSine',
+			complete: () => {
+				this.animate();
+			},
+		});
 	}
 
 	drawLine(from, to, color, width = 1) {
-		this.ctx.strokeStyle =color;
+		this.ctx.strokeStyle = color;
 
 		this.ctx.beginPath();
 		this.ctx.lineWidth = width;
@@ -113,7 +119,7 @@ class Stage {
 		this.ctx.closePath();
 	}
 
-	drawPoint (point) {
+	drawPoint(point) {
 		const { from, to } = this.line;
 		const wh = this.canvas.width * 0.5;
 		const hh = this.canvas.height * 0.5;
@@ -141,40 +147,52 @@ class Stage {
 
 		this.ctx.save();
 		this.ctx.globalAlpha = point.o;
-		this.drawLine(point, { x: toX, y: toY }, this.lineColor, lineWidth);
+		this.drawLine(point, { x: toX, y: toY }, this.options.lineColor, lineWidth);
 		this.ctx.restore();
 	};
 
+	updateSeparator() {
+		this.line.from.x = this.widthHalf + (Math.cos(this.rotation) * this.width);
+		this.line.from.y = this.heightHalf + (Math.sin(this.rotation) * this.width);
+		this.line.to.x = this.widthHalf + (Math.cos(this.rotation + Math.PI) * this.width);
+		this.line.to.y = this.heightHalf + (Math.sin(this.rotation + Math.PI) * this.width);
+	}
 
 	run() {
+		const { hypo } = this;
+
 		this.ctx.globalCompositeOperation = 'source-over';
-		this.ctx.fillStyle = this.backgroundColor;
+		this.ctx.fillStyle = this.options.backgroundColor;
 		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+		this.updateSeparator();
 
 		this.points.forEach((p, i) => this.drawPoint(p, i));
 
-		const rectWidth = this.heightHalf;
-
 		this.ctx.save();
 		this.ctx.translate(this.widthHalf, this.heightHalf);
-		this.ctx.rotate(this.mouseAngle + (Math.PI / 4));
+		this.ctx.rotate(this.rotation);
 		this.ctx.globalCompositeOperation = 'difference';
-		this.ctx.fillStyle = '#fff';
-		this.ctx.fillRect(-rectWidth / 2, -rectWidth / 2, rectWidth, rectWidth);
+		this.ctx.fillStyle = this.options.pointColor1;
+		this.ctx.fillRect(-hypo / 2, 0, hypo, hypo);
 		this.ctx.restore();
+
+		const circleRadius = 10;
+		this.ctx.beginPath();
+		this.ctx.fillStyle = '#40d4ad';
+		this.ctx.arc(this.widthHalf, this.heightHalf, circleRadius, 0, TAU);
+		this.ctx.fill();
+		this.ctx.closePath();
+
+		const circleRadiusInner = circleRadius * 0.5;
+		this.ctx.beginPath();
+		this.ctx.fillStyle = this.options.backgroundColor;
+		this.ctx.arc(this.widthHalf, this.heightHalf, circleRadiusInner, 0, TAU);
+		this.ctx.fill();
+		this.ctx.closePath();
 
 		requestAnimationFrame(() => this.run());
 	}
-
-	static getPalette(colors) {
-		return colors[Math.floor(Math.random() * colors.length)];
-	}
-
-	static getColors() {
-		return fetch('//unpkg.com/nice-color-palettes@2.0.0/100.json')
-			.then(res => res.json());
-	}
-
 }
 
 const stage = new Stage(canvas, window.innerWidth, window.innerHeight);

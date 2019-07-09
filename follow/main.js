f6acd659a9dffebea182f23da039c728524da8f9
Pim <pim@vicompany.nl>
2019-07-09 14:00:29 +0200

    movers

--- follow/main.js
+++ follow/main.js
@@ -1,10 +1,24 @@
 import Vector from '//rawgit.com/pimskie/vector/master/vector.js';
-import { wrappBBox, angleBetween } from '//rawgit.com/pimskie/utils/master/utils.js';
+import { wrappBBox, distanceBetween } from '//rawgit.com/pimskie/utils/master/utils.js';
 
 const TAU = Math.PI * 2;
 const simplex = new SimplexNoise();
 
-const degrees = r => r * (180 / Math.PI);
+const painting = {
+	drawSelf(context, position, color = '#000') {
+		context.save();
+		context.translate(position.x, position.y);
+
+		context.beginPath();
+		context.fillStyle = color;
+		context.arc(0, 0, 5, 0, TAU);
+		context.fill();
+		context.closePath();
+
+		context.restore();
+	}
+};
+
 
 class Stage {
 	constructor(canvas, width, height) {
@@ -26,68 +40,92 @@ class Stage {
 		this.canvas.width = this.width;
 		this.canvas.height = this.height;
 	}
+
+	getRandomPosition() {
+		return new Vector(this.width * Math.random(), this.height * Math.random());
+	}
 }
 
 class Mover {
-	constructor(position) {
+	constructor(position, target) {
 		this.position = position;
+		this.target = target;
+
+		this.painting = painting;
 
 		this.velocity = new Vector();
-		this.velocity.length = 1;
+		this.velocity.length = 0.5 + (Math.random());
 		this.velocity.angle = Math.random() * TAU;
 
-		this.target = new Vector(Math.random() * window.innerWidth, Math.random() * window.innerHeight);
+		this.turnSpeed = 0.1 + ((Math.random() * 0.9)) * 0.1;
 	}
-	getAngle() {
-		const turnSpeed = 0.05;
 
-		const target = angleBetween(this.position, this.target);
+	getAngleDifference(current, target) {
+		let angle = target - current;
+		angle = (angle + Math.PI) % TAU - Math.PI;
+
+		return angle;
+	}
+
+	moveTowardsAngle(targetPosition) {
+		const target = Math.atan2(targetPosition.y - this.position.y, targetPosition.x - this.position.x);
 		const current = this.velocity.angle;
-		let difference = current - target;
 
-		if (difference % Math.PI >= Math.PI) {
-			difference -= difference % Math.PI;
-		}
+		const deltaAngle = this.getAngleDifference(current, target);
 
-		if (Math.abs(target - current) <= turnSpeed) {
-			console.log('on target');
-
+		if (-this.turnSpeed < deltaAngle && deltaAngle < this.turnSpeed) {
 			return target;
 		}
 
-		console.log(difference)
-
-		return current - Math.sign(difference) * turnSpeed;
+		return this.moveTowards(current, current + deltaAngle, this.turnSpeed);
 	}
 
-	update(stageWidth, stageHeight) {
-		this.velocity.angle = this.getAngle();
+	moveTowards(current, target, turnSpeed) {
+		if (Math.abs(target - current) <= turnSpeed) {
+			return target;
+		}
+
+		return current + Math.sign(target - current) * turnSpeed;
+	}
+
+	update(stageWidth, stageHeight, targets) {
+		wrappBBox(this.position, stageWidth, stageHeight);
+
+		this.target = Mover.getClosest(this.position, targets);
+		this.velocity.angle = this.moveTowardsAngle(this.target.position);
 
 		this.position.addSelf(this.velocity);
 	}
 
 	draw(context) {
-		context.save();
-		context.translate(this.position.x, this.position.y);
+		this.painting.drawSelf(context, this.position, '#ff0000');
+	}
 
-		context.beginPath();
-		context.fillStyle = '#000';
-		context.arc(0, 0, 5, 0, TAU);
-		context.fill();
-		context.closePath();
+	static getClosest(position, others) {
+		let [closest] = others;
 
-		context.restore();
+		others.forEach((other) => {
+			if (distanceBetween(position, other.position) < distanceBetween(position, closest.position)) {
+				closest = other;
+			}
+		});
 
-		context.save();
-		context.translate(this.target.x, this.target.y);
+		return closest;
+	}
+}
 
-		context.beginPath();
-		context.fillStyle = '#ff0000';
-		context.arc(0, 0, 5, 0, TAU);
-		context.fill();
-		context.closePath();
+class Target {
+	constructor(position) {
+		this.position = position;
+		this.painting = painting;
+	}
 
-		context.restore();
+	update(stageWidth, stageHeight) {
+		wrappBBox(this.position, stageWidth, stageHeight);
+	}
+
+	draw(context) {
+		this.painting.drawSelf(context, this.position, '#000');
 	}
 }
 
@@ -97,16 +135,22 @@ const stage = new Stage(
 	window.innerHeight
 );
 
-const movers = new Array(1)
-	.fill()
-	.map(() => new Mover(new Vector(stage.width * 0.5, stage.height * 0.5)));
+
+const numMovers = 30;
+const movers = new Array(numMovers).fill().map(() => new Mover(stage.getRandomPosition()));
+const targets = new Array(numMovers).fill().map(() => new Target(stage.getRandomPosition()));
 
 const loop = () => {
 	stage.clear();
 
 	movers.forEach((mover) => {
-		mover.update(stage.width, stage.height);
+		mover.update(stage.width, stage.height, targets);
 		mover.draw(stage.context);
+	});
+
+	targets.forEach((target) => {
+		target.update(stage.width, stage.height);
+		target.draw(stage.context);
 	});
 
 	requestAnimationFrame(loop);

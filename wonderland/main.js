3cd1ccf300b5e2f1db0db992605e826f96e44658
Pim <pim@vicompany.nl>
2019-08-20 16:13:51 +0200

    working on forces

--- wonderland/main.js
+++ wonderland/main.js
@@ -1,6 +1,17 @@
 const simplex = new SimplexNoise(0.3);
 const TAU = Math.PI * 2;
 
+// const PALETTE = ['#FF9F1C', '#ffbf69', '#ffffff', '#cbf3f0', '#2ec4b6'];
+// const PALETTE = ['#c5f0a4', '#35b0ab', '#226b80', '#f34573'];
+const PALETTE = [
+	[94, 72, 79],
+	[178, 54, 45],
+	[193, 58, 32],
+	[344, 88, 61],
+];
+
+const clamp = (value, min, max) => Math.max(min, Math.min(value, max));
+
 class Stage {
 	constructor(canvas, width, height) {
 		this.canvas = canvas;
@@ -41,30 +52,35 @@ class Stage {
 }
 
 class Segment {
-  constructor(position, index, lightness) {
+  constructor(position, index) {
     const { x, y } = position;
 
     this.position = { x, y };
     this.positionPrevious = { x, y };
 		this.index = index;
-		this.lightness = lightness;
 
-    this.length = 1;
+    this.length = 2;
 
-    this.phase = this.index * 0.01;
-    this.speed = 0.001;
-  }
+    this.phase = this.index * 0.001;
+		this.speed = 0.001;
+
+		this.acceleration = { x: 0, y: 0 };
+	}
+
+	applyForce(force) {
+
+	}
 
   update() {
-    const volatilityX = 0.002;
-    const volatilityY = 0.001;
-    const multiplier = 1;
+    const volatilityX = 0.0001;
+    const volatilityY = 0.0005;
 
-    const noise = simplex.noise2D(this.phase, this.position.y * volatilityY, this.phase) * multiplier;
+    const noise = simplex.noise3D(this.position.x * volatilityX, this.position.y * volatilityY, this.phase);
+		const angleX = clamp(noise * Math.PI, -Math.PI / 2, Math.PI / 2);
 
     const velocity = {
-      x: 1, //  Math.cos(noise * Math.PI) * this.length,
-      y: Math.sin(noise * (Math.PI / 2)) * this.length,
+      x: Math.cos(angleX) * this.length,
+      y: Math.sin(noise * (Math.PI)) * this.length,
     };
 
     this.positionPrevious.x = this.position.x;
@@ -74,7 +90,7 @@ class Segment {
     this.position.y += velocity.y;
 
     this.phase += this.speed;
-  }
+	}
 }
 
 class Scene {
@@ -84,7 +100,18 @@ class Scene {
     this.segmentSpacing = this.stage.height / this.numSegments;
 
     this.segments = [];
-    this.rafId = null;
+		this.rafId = null;
+
+		this.forces = [
+			{
+				position: { x: this.stage.width * 0.25, y: 0 },
+				direction: { x: 1, y: 1 },
+			},
+			{
+				position: { x: this.stage.centerX, y: this.stage.height },
+				direction: { x: 1, y: -1 },
+			}
+		];
   }
 
   reset() {
@@ -94,6 +121,7 @@ class Scene {
 
   generate() {
 		const { stage: { centerY } } = this;
+
     this.segments = new Array(this.numSegments).fill().map((_, i) => {
 			const x = 25;
 			const y = i * this.segmentSpacing;
@@ -104,37 +132,53 @@ class Scene {
     });
   }
 
-  draw(point) {
-    const { context } = this.stage;
-    const { position, positionPrevious, lightness } = point;
+	connect(from, to, [h, s, l]) {
+		const { positionPrevious: topLeft, position: topRight } = from;
+		const { positionPrevious: bottomLeft, position: bottomRight } = to;
+		const { context } = this.stage;
+		const color = `hsl(${h}, ${s}%, ${l}%)`;
 
-    context.save();
 		context.beginPath();
-		context.lineWidth = 5;
-		context.strokeStyle = `hsl(0, 100%, ${40 + lightness}%)`;
-    context.moveTo(positionPrevious.x, positionPrevious.y);
-    context.lineTo(position.x, position.y);
-    context.stroke();
-    context.closePath();
-  }
+		context.strokeStyle = color;
+		context.fillStyle = color;
+		context.moveTo(topLeft.x, topLeft.y);
+		context.lineTo(topRight.x, topRight.y);
+		context.lineTo(bottomRight.x, bottomRight.y);
+		context.lineTo(bottomLeft.x, bottomLeft.y);
+		context.lineTo(topLeft.x, topLeft.y);
+		context.stroke();
+		context.fill();
+		context.closePath();
+	}
 
   run() {
-    this.segments.forEach((s) => {
-      s.update();
-      this.draw(s);
-    });
+    for (let i = 0; i < this.segments.length; i++) {
+			const segment = this.segments[i];
+
+			segment.applyForce(this.forces[0]);
+			segment.update();
+
+			if (i < this.segments.length - 1) {
+				const segmentNext = this.segments[i + 1];
+				const colorIndex = (i + 1 + PALETTE.length) % PALETTE.length;
+				const hsl = PALETTE[colorIndex];
+
+				this.connect(segment, segmentNext, hsl);
+			}
+		}
 
     this.rafId = requestAnimationFrame(() => this.run());
   }
 }
 
 const stage = new Stage(document.querySelector('.js-canvas'), window.innerWidth, window.innerHeight);
-const scene = new Scene(stage, 20);
+const scene = new Scene(stage, 100);
 scene.generate();
 scene.run();
 
 window.addEventListener('resize', () => {
 	stage.setSize(window.innerWidth, window.innerHeight);
+	scene.reset();
 });
 
 document.body.addEventListener('click', () => {

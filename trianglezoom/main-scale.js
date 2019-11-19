4604fb14b9c2a5269b92b34e09634f4bdfcf8a8b
Pim <pim@vicompany.nl>
2019-11-19 10:40:57 +0100

    zoom space

--- trianglezoom/main-scale.js
+++ trianglezoom/main-scale.js
@@ -1,88 +1,97 @@
 const simplex = new SimplexNoise();
 
-const distanceBetween = (x1, y1, x2, y2) => Math.hypot(x2 - x1, y2 - y1);
-const map = (value, start1, stop1, start2, stop2) => ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+const randomBetween = (min, max) => ~~(Math.random() * (max - min) + min);
 
 const randomHue = () => Math.random() * 360;
-const noiseColor = (hue, phase) => `hsl(${hue + (100 * phase)}, 100%, 50%)`;
+const noiseColor = (hue, phase) => `hsl(${hue + (80 * phase)}, 100%, 60%)`;
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = 500;
-const height = 500;
-
-const midX = width * 0.5;
-const midY = height * 0.5;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
 
 let phase = Math.PI * 0.5;
-const phaseSpeed = 0.009;
+const phaseSpeed = 0.005;
 
-const decay = 0.96;
-const grow = 1 + (1 - decay);
+let size = 1;
+let sides = 4;
 
-let lineWidth = 0.5;
-let size = 10;
-let sizeModifier = grow;
 let isOdd = true;
-let up = true;
+let isUp = true;
 
 let otherHue = randomHue();
 let otherColor = noiseColor();
 
-const clear = (ctx) => {
-	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-};
-
 const reset = () => {
-	clear(ctx);
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-	size = 10;
+	size = 1;
+	sides = randomBetween(3, 6);
 };
 
-const draw = (ctx, size, lineWidth, color) => {
+const draw = (ctx, path, lineWidth, color, opacity, phase) => {
+	ctx.globalAlpha = opacity;
 	ctx.save();
 	ctx.fillStyle = color;
 	ctx.strokeStyle = color;
 	ctx.lineWidth = lineWidth;
 
-	ctx.translate(midX, midY);
-	ctx.rotate((Math.PI * 0.25));
+	ctx.translate(width >> 1, height >> 1);
+	ctx.rotate(phase);
 	ctx.beginPath();
-	ctx.rect(-size * 0.5, -size * 0.5, size, size);
-	ctx.stroke();
+
+	path.forEach(({ x, y }, i) => ctx[i === 0 ? 'moveTo' : 'lineTo'](x, y))
+
 	ctx.closePath();
+	ctx.stroke();
 	ctx.restore();
 };
 
-ctx.globalCompositeOperation = 'source-over';
+const getPath = (sides, size) => {
+	const step = Math.PI * 2 / sides;
+
+	return new Array(sides).fill().map((_, i) => ({
+		x: Math.cos(step * i) * size,
+		y: Math.sin(step * i) * size,
+	}));
+};
 
 const loop = () => {
+	ctx.fillStyle = 'rgba(0, 0, 0, 0.005)';
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
+	const max = Math.min(width, height);
 	const noise = simplex.noise2D(size * 0.001, phase);
 	const color = isOdd ? '#000' : noiseColor(otherHue, noise);
-	const percent = size / width;
-	const lineWidth = 1 + (10 * percent);
+	const percent = Math.max(0, size / max);
 
-	const amount = 5 + (10 * (1 - percent));
-	if (up) {
-		size += amount;
-	} else {
-		size -= amount;
+	const amount = 3 + (4 * (1 - percent));
+	const lineWidth = amount * 0.75;
+
+	const path = getPath(sides, size);
+
+	draw(ctx, path, lineWidth, color, 1 - percent, phase);
+
+	if (size > max || size < 0) {
+		isUp = !isUp;
+
+		sides = randomBetween(3, 6);
 	}
 
-	draw(ctx, size, lineWidth, color);
-
-	if (size > width || size < 10) {
-		up = !up;
-
+	if (size < 0) {
 		otherHue = randomHue();
 	}
 
 	isOdd = !isOdd;
+
+	size = isUp
+		? size + amount
+		: size - amount;
+
 	phase += phaseSpeed;
 
 	requestAnimationFrame(loop);

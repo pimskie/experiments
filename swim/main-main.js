9499047c39958160e27f555bc546858839f1f76b
Pim <pim@vicompany.nl>
2019-12-18 15:27:57 +0100

    fuck this shit

--- swim/main-main.js
+++ swim/main-main.js
@@ -1,15 +1,16 @@
 const simplex = new SimplexNoise();
 
-const distanceBetween = (vec1, vec2) => Math.hypot(vec2.x - vec1.x, vec2.y - vec1.y);
 const clamp = (value, min, max) => Math.max(min, Math.min(value, max));
 
+const getDistanceBetween = (vec1, vec2) => Math.hypot(vec2.x - vec1.x, vec2.y - vec1.y);
 const getAngleDifference = (x, y) => Math.atan2(Math.sin(x - y), Math.cos(x - y));
 const getAngleBetween = (vec1, vec2) => Math.atan2(vec2.y - vec1.y, vec2.x - vec1.x);
 
 const canvas = document.querySelector('.js-canvas');
 const ctx = canvas.getContext('2d');
 
-const TAU = Math.PI * 2;
+const PI = Math.PI;
+const TAU = PI * 2;
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -27,46 +28,52 @@ const phaseSpeed = 0.005;
 const pointer = { x: 0, y: 0 };
 const position = { x: midX, y: midY };
 
-const numPoints = 2;
+const numStickers = 100;
+const numPoints = 100;
 const radius = 100;
 
-const drawForm = (ctx) => {
-	ctx.save();
-	ctx.translate(position.x, position.y);
 
-	for (let i = 0; i < numPoints; i++) {
-		const angle = TAU / numPoints * i;
-		const originalX = Math.cos(angle) * radius;
-		const originalY = Math.sin(angle) * radius;
+const draw = (ctx) => {
+	for (let q = 0; q < numStickers; q++) {
+		const stickerRadius = 200 + (q * 50);
 
-		const pointerDistance = distanceBetween(pointer, { x: originalX, y: originalY });
-		const pointerAngle = getAngleBetween(pointer, { x: originalX, y: originalY });
-		const force = (pointerDistance / hypot);
-		const newX = Math.cos(angle) * (radius * force);
-		const newY = originalY;
+		ctx.save();
+		ctx.translate(position.x, position.y);
 
 		ctx.beginPath();
 		ctx.strokeStyle = 'black';
-		ctx.arc(newX, newY, 5, 0, TAU);
+
+		for (let i = 0; i < numPoints; i++) {
+			const angle = TAU / numPoints * i;
+			const originalX = Math.cos(angle) * radius;
+			const originalY = Math.sin(angle) * radius;
+
+			const distance = getDistanceBetween(pointer, { x: originalX, y: originalY });
+			const forceDistance = 1 - clamp(distance / 500, 0, 1);
+			const forceX = clamp((pointer.x - originalX) / midX, -1, 1) * forceDistance;
+			const forceY = clamp((pointer.y - originalY) / midY, -1, 1) * forceDistance;
+
+			const ampX = forceX * stickerRadius;
+			const ampY = forceY * stickerRadius;
+
+			const newX = originalX - ampX;
+			const newY = originalY - ampY;
+
+			const m = i === 0 ? 'moveTo' : 'lineTo';
+
+			ctx[m](newX, newY);
+		}
+
 		ctx.closePath();
 		ctx.stroke();
+		ctx.restore();
 	}
-
-	// ctx.beginPath();
-	// ctx.strokeStyle = 'black';
-	// ctx.moveTo(x, y);
-	// ctx.lineTo(trailToX, trailToY);
-	// ctx.closePath();
-	// ctx.stroke();
-
-	ctx.restore();
-
 };
 
 const loop = () => {
 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-	drawForm(ctx);
+	draw(ctx);
 
 	phase += phaseSpeed;
 

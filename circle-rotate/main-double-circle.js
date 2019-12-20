0d73a9dafb846b3eba9f45401cf2f0e4155b9e17
Pim <pim@vicompany.nl>
2019-12-20 16:11:50 +0100

    circle circle

---
+++ circle-rotate/main-double-circle.js
@@ -0,0 +1,48 @@
+const canvas = document.querySelector('.js-canvas');
+const ctx = canvas.getContext('2d');
+
+const TAU = Math.PI * 2;
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const midX = width >> 1;
+const midY = height >> 1;
+
+canvas.width = width;
+canvas.height = height;
+
+let phase = 0;
+const numCircles = 50;
+const radius = 200;
+
+const loop = () => {
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	for (let i = 0; i < numCircles; i++) {
+		const angle = (TAU / numCircles * i); // + phase;
+
+		ctx.save();
+		ctx.translate(midX, midY);
+		ctx.rotate(angle);
+
+		// const radiusX = radius * 0.25;
+		const radiusX = (radius * 0.25) + (Math.cos(Math.PI * 2 * (i / numCircles) + phase) * (radius * 0.25));
+		const radiusY = radius;
+
+		ctx.beginPath();
+		ctx.ellipse(0, 0, radiusX, radiusY, 0, 0, TAU, false);
+		ctx.closePath();
+		ctx.stroke();
+
+		ctx.restore();
+	}
+
+
+	phase += 0.01;
+
+	requestAnimationFrame(loop);
+};
+
+
+loop();
---
+++ circle-rotate/main.js
@@ -0,0 +1,50 @@
+const canvas = document.querySelector('.js-canvas');
+const ctx = canvas.getContext('2d');
+
+const PI = Math.PI;
+const TAU = PI * 2;
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const midX = width >> 1;
+const midY = height >> 1;
+
+canvas.width = width;
+canvas.height = height;
+
+let phase = 0;
+const numCircles = 20;
+const radius = 200;
+const radiusHalf = radius * 0.5;
+
+const loop = () => {
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	for (let i = 0; i < numCircles; i++) {
+		const angle = (PI / numCircles) * i;
+
+		ctx.save();
+		ctx.translate(midX, midY);
+		ctx.rotate(angle);
+
+		const circleCos = Math.abs(Math.cos((i / numCircles * 2) * PI + phase));
+		const radiusX = radius
+		const radiusY = radiusHalf * circleCos;
+
+		ctx.beginPath();
+		ctx.ellipse(0, 0, radiusX, radiusY, 0, 0, TAU, false);
+		ctx.closePath();
+		ctx.stroke();
+
+		ctx.restore();
+	}
+
+
+	phase += 0.01;
+
+	requestAnimationFrame(loop);
+};
+
+
+loop();

874283f1a6f8a5161b7c84286f2b2b5a7ce49be7
Pim van Die <pim@vicompany.nl>
2022-05-12 14:20:19 +0200

    space

--- para-circle/main.js
+++ para-circle/main.js
@@ -5,6 +5,7 @@ const TAU = Math.PI * 2;
 const W = 500;
 const H = W;
 const R = W >> 1;
+const R2 = R * 0.25;
 
 const NUM_LINES = 100;
 const LINE_ANGLE_STEP = TAU / NUM_LINES;
@@ -19,16 +20,16 @@ const drawCircle = (scale = 1, iteration) => {
 	ctx.save();
 	ctx.translate(W >> 1, H >> 1);
 	ctx.scale(scale, scale);
+	ctx.strokeStyle = 'rgba(50, 50, 50, 0.75)';
 
 	for (let i = 0; i < NUM_LINES; i++) {
 		const angle = LINE_ANGLE_STEP * i;
 		const LINE_DEGREE_TO = LINE_ANGLE_STEP * 2 * (iteration * 0.2);
-
+		const angleIteration = iteration * 0.05;
 
 		ctx.beginPath();
-		ctx.moveTo(Math.cos(angle) * R, Math.sin(angle) * R);
-		ctx.lineTo(Math.cos(angle + LINE_DEGREE_TO) * R, Math.sin(angle + LINE_DEGREE_TO) * R);
-		ctx.lineTo(Math.cos(angle + LINE_WIDTH_ANGLE) * R, Math.sin(angle + LINE_WIDTH_ANGLE) * R);
+		ctx.moveTo(Math.cos(angle - angleIteration) * R, Math.sin(angle) * R);
+		ctx.lineTo(Math.cos(angle + angleIteration) * R2, Math.sin(angle - angleIteration) * R2);
 		ctx.closePath();
 		ctx.stroke();
 		ctx.fill();

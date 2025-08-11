26f9cbd750bd6c75dca2a8590b5ee243664c7222
Pim van Die <pim.vandie@iodigital.com>
2025-08-11 15:37:05 +0200

    fire layers

--- fire-sparks/main.js
+++ fire-sparks/main.js
@@ -3,36 +3,48 @@ const randomArrayValue = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const w = window.innerWidth;
 const h = window.innerHeight;
 
-const canvas = document.querySelector("canvas");
-canvas.width = w;
-canvas.height = h;
+const numLayers = 4;
 
-const ctx = canvas.getContext("2d");
+const ctxs = new Array(numLayers).fill(0).map((_, i) => {
+	const canvas = document.createElement("canvas");
 
-const noiseLayers = new Array(50).fill(0).map((_, i) => {
-	const simplex = new SimplexNoise();
-	return simplex;
+	canvas.width = w;
+	canvas.height = h;
+	canvas.style.setProperty("--layer", i * 10);
+
+	return canvas.getContext("2d");
 });
+
+ctxs.forEach((ctx) => {
+	document.body.appendChild(ctx.canvas);
+});
+
+const noiseLayers = new Array(ctxs.length)
+	.fill(0)
+	.map((_, i) => new SimplexNoise());
 
 let phase = 0;
 
-const sparks = new Array(50).fill(0).map((_, i) => ({
+const sparks = new Array(100).fill(0).map((_, i) => ({
 	x: -300 * Math.random(),
 	y: Math.random() * h,
 	size: 3,
 	life: 1,
 	speed: 7 + 5 * Math.random(),
-	decay: 0.99 + 0.01 * Math.random(),
+	decay: 0.989 + 0.01 * Math.random(),
 	noiseLayer: randomArrayValue(noiseLayers),
+	ctx: randomArrayValue(ctxs),
 }));
 
 const clean = () => {
-	ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	ctxs.forEach((ctx) => {
+		ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	});
 };
 
 const update = (spark, phase) => {
-	const scale = 0.0001;
+	const scale = 0.001;
 	const noiseLayer = spark.noiseLayer;
 	const noise = noiseLayer.noise3D(spark.x * scale, spark.y * scale, phase, 0);
 	const angleY = noise * Math.PI;
@@ -42,26 +54,24 @@ const update = (spark, phase) => {
 	spark.x += spark.speed * spark.life;
 	spark.y += Math.sin(angleY) * (spark.speed * spark.life * spark.life * 0.5);
 	spark.speed *= spark.decay;
-
-	if (spark.y > h) {
-		spark.y = 0;
-	}
-
-	if (spark.y < 0) {
-		spark.y = h;
-	}
 };
 
-const draw = ({ x, y, size, life }) => {
+const draw = ({ ctx, x, y, size, life }) => {
 	ctx.save();
-
 	ctx.translate(x, y);
 
-	// Fire spark color gradient: yellow -> orange/red
 	const r = 255;
 	const g = 255 * life;
 	const b = 0;
 
+	// shadow
+	ctx.fillStyle = `rgba(0, 0, 0, 1)`;
+	ctx.beginPath();
+	ctx.arc(2, 2, size * life, 0, Math.PI * 2);
+	ctx.fill();
+	ctx.closePath();
+
+	// spark
 	ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${life})`;
 	ctx.beginPath();
 	ctx.arc(0, 0, size * life, 0, Math.PI * 2);

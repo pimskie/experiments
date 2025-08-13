30f8750fc86229eda3a39156db577a6e06c59f6e
Pimskie <pimskiedev@gmail.com>
2025-08-13 20:05:22 +0200

    foofoo

--- three/rectangle-noise-field/main.js
+++ three/rectangle-noise-field/main.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { createNoise3D, createNoise2D } from "simplex-noise";
+import { createNoise2D } from "simplex-noise";
+
+import GUI from "lil-gui";
 
 const randomArrayValue = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const palette = ["#780000", "#c1121f", "#fdf0d5", "#003049", "#669bbc"];
@@ -17,43 +19,71 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.y = 30;
 camera.position.z = 30;
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+const color = 0xffffff;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+const updateCubes = (config, cubes) => {
+  const { noiseScale } = config;
+
+  cubes.forEach(({ mesh, height }, index) => {
+    const { x, y } = mesh.position;
+    mesh.rotation.set(0, noise2D(x * noiseScale, y * noiseScale), 0);
+  });
+};
+
 const spread = 20;
-const noiseScale = 0.1;
-document.body.appendChild(renderer.domElement);
 
-for (let count = 0; count < 300; count++) {
+const config = {
+  noiseScale: 0.01,
+};
+
+const cubes = new Array(300).fill(0).map((_, index) => {
   const x = -spread / 2 + Math.random() * spread;
-  const z = Math.random() * spread;
+  const z = Math.random() * (spread * 2);
 
-  const noiseValue = noise2D(x * noiseScale, z * noiseScale);
-  const noiseValue2 = noise2D(z * noiseScale, x * noiseScale);
-
-  const height = 7 + 7 * noiseValue;
-  const w = 1 + noiseValue2 * 1;
+  const height = 5 + Math.random() * 5;
+  const w = 2 + Math.random();
+  const d = 2 + Math.random();
 
   const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(w, height, w),
-    new THREE.MeshBasicMaterial({
+    new THREE.BoxGeometry(w, height, d),
+    new THREE.MeshPhongMaterial({
       color: randomArrayValue(palette),
     })
   );
 
   cube.position.set(x, height * 0.5, z);
-  cube.rotation.set(0, noise2D(count * 0.001, 1), 0);
 
   scene.add(cube);
-}
 
-const transformCard = (tick) => {};
+  return {
+    mesh: cube,
+    height,
+    scale: 1,
+  };
+});
+
+updateCubes(config, cubes);
+
+const gui = new GUI();
+gui
+  .add(config, "noiseScale", 0.01, 0.3, 0.01)
+  .onChange((val) => updateCubes(config, cubes));
+
+document.body.appendChild(renderer.domElement);
 
 function animate() {
   const elapsedTime = clock.getElapsedTime();
-  transformCard(elapsedTime);
 
   controls.update();
   renderer.render(scene, camera);
